bcff24c1277f73b705856939e21ec76667c7de9b
woowenjun99 <woowenjun99@gmail.com>
2024-01-10 17:29:22 +0800

    Add in explanation for cocktail.py

--- src/Furious Cocktail/cocktail.py
+++ src/Furious Cocktail/cocktail.py
@@ -1,3 +1,16 @@
+"""
+Topic: Greedy + Sorting
+
+Explanation:
+
+We will drink the potion that can last the longest first because if we drink 
+the shortest first, the effect will wear off. Notice that whether the potion
+can last the whole game is dependent on the drinking time of the other potions.
+This means that for potions[i], it will be dependent on (n - 1 - i) * t.
+
+Time complexity: O(n log n)
+Space complexity: O(n)
+"""
 import sys
 
 input = sys.stdin.readline
