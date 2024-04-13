e67610cbfd97ee9f302c9b3454bc4b382098f131
woowenjun99 <woowenjun99@gmail.com>
2024-04-13 18:47:45 +0800

    Update main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
-#include <string>
+
+void solve() {}
 
 int main(void) {
+    freopen("in.txt", "r", stdin);
     std::ios::sync_with_stdio(0); 
 	std::cin.tie(0); 
 	std::cout.tie(0);
